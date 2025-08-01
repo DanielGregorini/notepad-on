c7dbcd300d737a86f9640b8d0742b39bde4de52c
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2025-08-01 14:27:50 -0300

    inicial

---
+++ backend/notepad-on-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/DanielGregorini/notepad-on
+
+go 1.24.5
---
+++ frontend/notepad-on/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Notepad on",
+  description: "A simple notepad application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
